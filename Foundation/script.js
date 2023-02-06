03ce7cd52a19bc5e412d4a30704a61a2e1dd9180
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2023-02-06 21:55:55 +0200

    Add files via upload

---
+++ Foundation/script.js
@@ -0,0 +1,27 @@
+//// Exercise Fixin sync code with async => setTimeout WebApi
+// const list = new Array(60000).join('1.1').split('.');
+
+// function removeItemsFromList() {
+//   var item = list.pop();
+
+//   if (item) {
+//     setTimeout(removeItemsFromList, 0);
+//   }
+// }
+
+// removeItemsFromList();
+///////////////////////////////////////////////
+
+function printName() {
+  return 'Andrei Neagoie';
+}
+
+function findName() {
+  return printName();
+}
+
+function sayMyName() {
+  return findName();
+}
+
+console.log(sayMyName());

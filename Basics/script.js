8c70d8586316390fd299c69270bd5cb10e974be4
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2023-02-07 18:45:04 +0200

    Add files via upload

---
+++ Basics/script.js
@@ -0,0 +1,14 @@
+//// JS Types
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null = there is nothing in the object; NO properties
+// 6. Symbol
+// 7. Object = collections of properties. Array is just a kind of object
+
+// JS Conditionals:
+// if else if else switch ternary operator
+
+// JS Logical Operators:
+// && || !

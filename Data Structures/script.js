8546da31012d7f6c62c27b822aaca47fc24c1993
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2023-02-21 11:53:59 +0200

    Merged Sorted Arrays
    Exercise: Merge Sorted Arrays

--- Data Structures/script.js
+++ Data Structures/script.js
@@ -17,6 +17,10 @@
 // arrStrings.shift(); // 0(n)
 // console.log(arrStrings);
 
+// Sort Array:
+// const nums = [1, 2, 4, 67, 8, 90];
+// console.log(nums.sort((a, b) => a - b)); // best practice
+
 //// Static: limited number of values vs Dynamic Arrays: copy and rebuild array
 // //// Array Implementation:
 // class MyArray {
@@ -100,6 +104,36 @@
 // }
 // console.log(reverse('first'));
 
-//// ES6 New syntax solution:
-const reversES6 = (str) => [...str].reverse().join('');
-console.log(reversES6('reversE'));
+// //// ES6 New syntax solution:
+// const reversES6 = (str) => [...str].reverse().join('');
+// console.log(reversES6('reversE'));
+
+//////////////////////////////////
+//// Exerces: Merge Sorted Arrays:
+
+function mergeSortedArrays(arr1, arr2) {
+  if (arr1.length === 0) return arr2;
+  if (arr2.length === 0) return arr1;
+
+  const mergedArray = [];
+  let arr1Item = arr1[0];
+  let arr2Item = arr2[0];
+  let i = 1;
+  let j = 1;
+
+  while (arr1Item || arr2Item) {
+    if (!arr2Item || arr1Item < arr2Item) {
+      mergedArray.push(arr1Item);
+      arr1Item = arr1[i];
+      i++;
+    } else {
+      mergedArray.push(arr2Item);
+      arr2Item = arr2[j];
+      j++;
+    }
+  }
+
+  return mergedArray;
+}
+
+console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));

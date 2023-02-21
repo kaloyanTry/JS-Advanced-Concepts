433fa26bdaf752bac75b2105fca6d2850ec6515d
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2023-02-21 10:05:52 +0200

    Data Structures JS
    Array Implementation

---
+++ Data Structures/script.js
@@ -0,0 +1,77 @@
+//// Data Strucutres
+
+// //// 1. Array (List)
+// const arrStrings = ['a', 'b', 'c', 'd'];
+
+// console.log(arrStrings);
+
+// arrStrings.push('e'); // 0(1) = Time Complexity
+// console.log(arrStrings);
+
+// arrStrings.pop(); // 0(1)
+// console.log(arrStrings);
+
+// arrStrings.unshift('first'); // 0(n)
+// console.log(arrStrings);
+
+// arrStrings.shift(); // 0(n)
+// console.log(arrStrings);
+
+//// Static: limited number of values vs Dynamic Arrays: copy and rebuild array
+//// Array Implementation:
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  get(index) {
+    return this.data[index];
+  }
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+    return item;
+  }
+  shift() {
+    const index = 0;
+    const firstItem = this.data[index];
+    this.shiftItems(index);
+    return firstItem;
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+const newArray = new MyArray();
+console.log(newArray);
+
+newArray.push('first');
+newArray.push('second');
+newArray.push('third');
+console.log(newArray);
+// newArray.pop(); // pop() = remove the last item
+// console.log(newArray.pop());
+
+// newArray.delete(1); // delete the item at index 1
+// console.log(newArray);
+
+newArray.shift(); // remove the first item
+console.log(newArray);
